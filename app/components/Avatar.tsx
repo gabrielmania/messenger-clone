31cad4ea3eff3aa4e9574bb66c3456844d2af005
Gabriel Mania <mania.gabb@gmail.com>
2023-05-25 09:42:00 +0800

    finished initial ui design of sidebar and navbar

--- app/components/Avatar.tsx
+++ app/components/Avatar.tsx
@@ -2,6 +2,7 @@
 
 import React from "react";
 import { User } from "@prisma/client";
+import Image from "next/image";
 
 interface AvatarProps {
   user?: User;
@@ -10,7 +11,14 @@ interface AvatarProps {
 const Avatar: React.FC<AvatarProps> = ({ user }) => {
   return (
     <div className="relative">
-      <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11"></div>
+      <div className="relative inline-block rounded-full overflow-hidden h-9 w-9 md:h-11 md:w-11">
+        <Image
+          alt="Avatar"
+          src={user?.image || "/images/placeholder.jpg"}
+          fill
+        />
+      </div>
+      <span className="absolute block rounded-full bg-green-500 ring-2 ring-white top-0 right-0 h-2 w-2 md:h-3 md:w-3" />
     </div>
   );
 };
